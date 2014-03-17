2f96aa741b8116c7a9085c5daa77c0470c10d505
unknown <Administrator@.(none)>
2014-03-17 12:09:54 +0100

    ovaj fercera

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InternetOglasnik::Application.config.secret_token = '729fe5c83d1f1206e4992d2a7e1e0956218c13288b853bf47bc24ec6d6f2fae263b3f1440c99f82c460c732f0e55e5f1a992111b6a752093e8b3a44b82c7eab3'
